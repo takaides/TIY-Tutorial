5f0f55716f768891ef383339cae33401ef08587f
takaides <takaides+github@gmail.com>
2017-06-06 18:11:12 -0400

    Completely Failing at life
    Nothing is working and I'm spending way too much time trying to fix what doesn't work instead of finishing adding content.

---
+++ Homework/main.js
@@ -0,0 +1,44 @@
+console.log("Logging from the console");
+
+var pD = document.getElementById('pD');
+
+var sF = document.getElementById('sF');
+
+var rV = document.getElementById('rV');
+
+var productDetails = document.getElementById('productDetails');
+
+var sizeAndFit = document.getElementById('sizeAndFit');
+
+var reviews = document.getElementById('reviews');
+
+// var trigger = document.getElementById('id');
+//
+// var modal = document.getElementById('id');
+
+// function addedItem(){
+//   // onClickEvent(
+//   //
+//   // )
+//
+// }
+function pD.addEventListener("hover", function productDetailsFun(){
+  console.log("Hiding Reviews and Size and Fit")
+  productDetails.visibility='visible';
+  reviews.visibility='hidden';
+  sizeAndFit.visibility='hidden';
+});
+
+function sF.addEventListener("hover", function sizeAndFitFun(){
+  console.log("Hiding Reviews and Product Details")
+  sizeAndFit.visibility='visible';
+  reviews.visibility='hidden';
+  productDetails.visibility='hidden';
+});
+
+function rV.addEventListener("hover", function Added Item(){
+  console.log("Hiding Product Details and Size and Fit")
+  reviews.visibility='visible';
+  productDetails.visibility='hidden';
+  sizeAndFit.visibility='hidden';
+});
