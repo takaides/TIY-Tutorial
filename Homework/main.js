b764c4ca4ca6e45c4c8a977799b9eb970b2ebbea
takaides <takaides+github@gmail.com>
2017-06-08 18:42:49 -0400

    End of Day 6
    Javascript works as currently intended, but does not have implementation to revert "buy Now" button back (was looking for a 'wait' function or something similar.

--- Homework/main.js
+++ Homework/main.js
@@ -12,17 +12,6 @@ var sizeAndFit = document.getElementById("sizeAndFit");
 
 var reviews = document.getElementById("reviews");
 
-// var trigger = document.getElementById('id');
-//
-// var modal = document.getElementById('id');
-
-// function addedItem(){
-//   // onClickEvent(
-//   //
-//   // )
-//
-// }
-
 pD.addEventListener("click", function productDetailsFun() {
   console.log("Hiding Reviews and Size and Fit")
   productDetails.style.display='block';
@@ -42,4 +31,24 @@ rV.addEventListener("click", function reviewFun() {
   reviews.style.display='block';
   productDetails.style.display='none';
   sizeAndFit.style.display='none';
+});
+
+var cart = document.getElementById('cart');
+
+var buyButton = document.getElementById('buyButton');
+
+var cartCount = 1;
+
+function changeBuyButton(){
+  buyButton.innerHTML = "Added to cart!";
+  
+}
+
+buyButton.addEventListener("click", function buyButtonClick(){
+  console.log("Buy Button clicked");
+  cart.innerHTML = '<img class="cart" src="images/cart.jpg">Shopping Cart ('+ cartCount++ + ')';
+
+  buyButton.innerHTML = "Added to cart!";
+  // var reset = setTimeout(buyButton.innerHTML = "BUY NOW!", 1500);
+  // clearTimeout(reset);
 });
