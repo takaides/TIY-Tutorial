98f85a66dddf8450ba1e24259ea1bd5a26c87e62
takaides <takaides+github@gmail.com>
2017-06-09 03:13:54 -0400

    Atom-beautify
    slightly changed readability of most valued files.

--- Homework/main.js
+++ Homework/main.js
@@ -14,23 +14,23 @@ var reviews = document.getElementById("reviews");
 
 pD.addEventListener("click", function productDetailsFun() {
   console.log("Hiding Reviews and Size and Fit")
-  productDetails.style.display='block';
-  reviews.style.display='none';
-  sizeAndFit.style.display='none';
+  productDetails.style.display = 'block';
+  reviews.style.display = 'none';
+  sizeAndFit.style.display = 'none';
 });
 
 sF.addEventListener("click", function sizeAndFitFun() {
   console.log("Hiding Reviews and Product Details")
-  sizeAndFit.style.display='block';
-  reviews.style.display='none';
-  productDetails.style.display='none';
+  sizeAndFit.style.display = 'block';
+  reviews.style.display = 'none';
+  productDetails.style.display = 'none';
 });
 
 rV.addEventListener("click", function reviewFun() {
   console.log("Hiding Product Details and Size and Fit")
-  reviews.style.display='block';
-  productDetails.style.display='none';
-  sizeAndFit.style.display='none';
+  reviews.style.display = 'block';
+  productDetails.style.display = 'none';
+  sizeAndFit.style.display = 'none';
 });
 
 var cart = document.getElementById('cart');
@@ -39,14 +39,14 @@ var buyButton = document.getElementById('buyButton');
 
 var cartCount = 1;
 
-function changeBuyButton(){
+function changeBuyButton() {
   buyButton.innerHTML = "Added to cart!";
-  
+
 }
 
-buyButton.addEventListener("click", function buyButtonClick(){
+buyButton.addEventListener("click", function buyButtonClick() {
   console.log("Buy Button clicked");
-  cart.innerHTML = '<img class="cart" src="images/cart.jpg">Shopping Cart ('+ cartCount++ + ')';
+  cart.innerHTML = '<img class="cart" src="images/cart.jpg">Shopping Cart (' + cartCount++ + ')';
 
   buyButton.innerHTML = "Added to cart!";
   // var reset = setTimeout(buyButton.innerHTML = "BUY NOW!", 1500);
